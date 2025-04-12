d7bb4f7874216b6610b05aabcb94e4bd02ef9748
garrettallen0 <garrett.allen13@gmail.com>
2025-04-11 22:59:38 -0700

    update sanityLive component

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,18 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
+import { createClient } from 'next-sanity';
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
+// Create a client with live preview enabled
+export const liveClient = createClient({
+  ...client.config(),
+  apiVersion: 'vX', // Use the latest API version
+  stega: {
+    enabled: true,
+    studioUrl: '/studio'
+  }
 });
+
+// Export the client for use in components
+export { liveClient as sanityFetch };
