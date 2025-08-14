6bc916191a30151c6fd603e890c071b7efd467f3
garrettallen0 <garrett.allen13@gmail.com>
2025-08-14 12:35:55 -0700

    add sanity images schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,8 +2,9 @@ import { type SchemaTypeDefinition } from 'sanity'
 import event from './event'
 import page from './page'
 import homepageImage from './homepageImage'
+import images from './images'
 
-export const schemaTypes = [event, page, homepageImage]
+export const schemaTypes = [event, page, homepageImage, images]
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: schemaTypes,
