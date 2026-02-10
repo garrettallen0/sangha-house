b7afa20b441dacd3943b8d9323f59b957d4afb8c
garrettallen0 <garrett.allen13@gmail.com>
2026-02-10 14:03:10 -0800

    add link to nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,6 +29,14 @@ export default function Navigation() {
               >
                 Events
               </Link>
+              <a
+                href="https://www.walkforpeacela.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-transparent text-indigo-700 hover:border-indigo-300 hover:text-indigo-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                🎉 Walk for Peace LA 🎉
+              </a>
               <Link
                 href="/contact"
                 className="border-transparent text-indigo-700 hover:border-indigo-300 hover:text-indigo-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
@@ -162,6 +170,14 @@ export default function Navigation() {
           >
             Events
           </Link>
+          <a
+            href="https://www.walkforpeacela.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-indigo-700 hover:bg-indigo-50 hover:border-indigo-300 hover:text-indigo-900"
+          >
+            🎉 Walk for Peace LA 🎉
+          </a>
           <Link
             href="/contact"
             className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-indigo-700 hover:bg-indigo-50 hover:border-indigo-300 hover:text-indigo-900"
